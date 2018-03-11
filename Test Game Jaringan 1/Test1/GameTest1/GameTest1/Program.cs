c1683d80a946291efc0f8e90a2a4c5b1c2fe5c3f
HaidarATN <34336797+HaidarATN@users.noreply.github.com>
2018-03-11 12:57:28 +0700

    Add files via upload
    Progress Minggu ini

---
+++ Test Game Jaringan 1/Test1/GameTest1/GameTest1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace GameTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Connect("127.0.0.1", 8000);
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int bytes = server.Receive(buffer);
+                Console.WriteLine(Encoding.Default.GetString(buffer, 0, bytes));
+            }
+        }
+    }
+}
---
+++ Test Game Jaringan 1/Test1/gameClient1/gameClient1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace gameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+                Socket newsock = new Socket(AddressFamily.InterNetwork,
+                                   SocketType.Stream, ProtocolType.Tcp);
+                newsock.Bind(localEndPoint);
+                newsock.Listen(10);
+                Socket client = newsock.Accept();
+
+                if (client.Connected)
+                {
+                    Console.WriteLine("client connected.");
+                }
+
+                string msg = "Who's there?";
+                byte[] buffer = new byte[msg.Count()];
+                buffer = Encoding.ASCII.GetBytes(msg);
+                client.Send(buffer);
+
+                while (client.Connected)
+                {
+                    Console.WriteLine("enter msg: ");
+                    string userMsg = Console.ReadLine();
+                    byte[] userBuffer = new byte[userMsg.Count()];
+                    userBuffer = Encoding.ASCII.GetBytes(userMsg);
+                    client.Send(userBuffer);
+
+                    Console.ReadKey();
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+        }
+    }
+}
